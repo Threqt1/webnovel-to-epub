92f25e2f7d474da926f53caf2d47e8afad721a5d
Krish Parikh <85196467+Threqt1@users.noreply.github.com>
2024-07-26 15:20:13 -0500

    fix woopread

--- src/scrapers/woopread.ts
+++ src/scrapers/woopread.ts
@@ -104,7 +104,7 @@ export default class WoopreadScraper extends Scraper {
                     });
                 }
             );
-            chapters = chapters.reverse().slice(0, 3);
+            chapters = chapters.reverse();
         }
 
         return chapters;
